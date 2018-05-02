a3ec8370b44353aac6b3013e27c5047aba03392c
Burak Akyalçın <burakakyalcin@mail.ru>
2018-05-02 09:40:30 +0300

    initial commit

---
+++ FirestoreChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirestoreChat
+//
+//  Created by Burak Akyalcin on 2.05.2018.
+//  Copyright © 2018 Burak Akyalcin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
